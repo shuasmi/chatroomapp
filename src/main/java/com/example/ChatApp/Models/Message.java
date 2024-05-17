2f9d4cd89ac8dc505c5757d8b8c5af2e95aa97f8
shuasmi <baraskarshubham272@gmail.com>
2024-05-17 20:04:40 +0530

    All files

---
+++ src/main/java/com/example/ChatApp/Models/Message.java
@@ -0,0 +1,33 @@
+package com.example.ChatApp.Models;
+
+public class Message {
+	
+	private String name;
+	
+	private String content;
+
+	public Message(String name, String content) {
+		super();
+		this.name = name;
+		this.content = content;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
